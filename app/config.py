27f34f7744daf1c4b7f1956eec4d0c42228d297f
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-03-20 02:35:36 -0500

    commit for everything else

--- app/config.py
+++ app/config.py
@@ -3,9 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env if it exists.
 
+
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME', 'admin')
+    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'Password123')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', './uploads')
