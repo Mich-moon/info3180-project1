20c801a8ebcf0cedb834b92870dc3ffdf7b97e83
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-03-20 04:09:45 -0500

    updated init file so migrate command works

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.models import Property
 
 app = Flask(__name__)
 app.config.from_object(Config)
