b5d7621e3a1614ffdd9cb7bb0072052e533c6b81
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-03-20 02:25:15 -0500

    added routes and functions

--- app/views.py
+++ app/views.py
@@ -5,8 +5,12 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
-from flask import render_template, request, redirect, url_for
+import os
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
+from werkzeug.utils import secure_filename
+from .forms import PropertyForm
+from app.models import Property
 
 
 ###
@@ -22,14 +26,83 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html')
 
+
+@app.route('/properties')
+def properties():
+    """Render page for displaying list of all properties."""
+    # results = Property.query.all()
+    results = db.session.query(Property).all()
+
+    properties = [{
+        "id": r.id,
+        "title": r.prop_title,
+        "desc": r.prop_desc,
+        "room_count": r.prop_room_count,
+        "bathroom_count": r.prop_bathroom_count,
+        "price": r.prop_price,
+        "type": r.prop_type,
+        "location": r.prop_location,
+        "photo": r.prop_photo,
+    } for r in results]
+
+    return render_template('properties.html', properties=properties)
+
+
+@app.route('/uploads/<string:filename>')
+def get_image(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER'][0:]), filename)
+
+
+@app.route('/properties/create', methods=["GET", "POST"])
+def create_property():
+    """Render form for adding a new property."""
+    form = PropertyForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+
+            prop_photo = form.photo.data
+            photo_filename = secure_filename(prop_photo.filename)
+            prop_photo.save(os.path.join(
+                os.environ.get('UPLOAD_FOLDER'), photo_filename
+            ))
+
+            prop = Property(
+                prop_title=form.title.data,
+                prop_desc=form.desc.data,
+                prop_room_count=form.room_count.data,
+                prop_bathroom_count=form.bathroom_count.data,
+                prop_price=form.price.data,
+                prop_type=form.type.data,
+                prop_location=form.location.data,
+                prop_photo=photo_filename
+            )
+            db.session.add(prop)
+            db.session.commit()
+
+            flash('New property added successfully', 'success')
+            return redirect(url_for('properties'))
+
+    return render_template('create_property.html', form=form)
+
+
+@ app.route('/properties/<propertyid>')
+def property(propertyid):
+    """Render page for displaying a single property."""
+    #prop = Property.query.get(int(propertyid))
+    prop = db.session.query(Property).get(int(propertyid))
+
+    return render_template('property.html', prop=prop)
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
 
 # Display Flask WTF errors as Flash messages
+
+
 def flash_errors(form):
     for field, errors in form.errors.items():
         for error in errors:
@@ -38,14 +111,15 @@ def flash_errors(form):
                 error
             ), 'danger')
 
-@app.route('/<file_name>.txt')
+
+@ app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
 
-@app.after_request
+@ app.after_request
 def add_header(response):
     """
     Add headers to both force latest IE rendering engine or Chrome Frame,
@@ -57,11 +131,11 @@ def add_header(response):
     return response
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="8080")
+    app.run(debug=True, host="0.0.0.0", port="8080")
